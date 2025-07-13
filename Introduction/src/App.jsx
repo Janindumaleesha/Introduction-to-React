f10301c21bbae5d49b2047adc576ac4c3cd8a63d
Janindumaleesha <janindu.maleesha@app360.lk>
2025-07-13 15:46:28 +0530

    first commit

---
+++ Introduction/src/App.jsx
@@ -0,0 +1,50 @@
+
+function App() {
+
+  const a = 60;
+
+  const fname = "Janindu";
+  const mname = "Maleesha";
+  const lname = "Ariyasinghe";
+  const age = "26";
+  const role = "Software Engineer";
+
+  const getFullName = (f,m,l) => {
+    return `${f} ${m} ${l}`;
+  }
+  
+  const arr = ["Apple", "Orange", "Mango"]; 
+
+  const lang = <ul>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>React</li>
+                  <li>JavaScript</li>
+               </ul>
+
+  const obj = {
+    fname: "Viraji",
+    age: 25 
+  } 
+
+  return (
+    <>
+      <div className="cont">
+        <h1>Janindu Maleesha</h1>
+        <p>{10*10}% for JSX</p>
+        <p>{a}</p>
+        <p>{a > 50 ? "Greater" : "Less"}</p>
+
+        <h1>Employee Details</h1>
+        <p>Full Name: {getFullName("Janindu", "Maleesha", "Ariyasinghe")}</p>
+        <p>Age: {age}</p>
+        <p>Role: {role}</p>
+        <p>{fname} like to eat {arr[0]}</p>
+        <p>{lang}</p>
+        <p>{obj.fname} is {obj.age} years old.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
