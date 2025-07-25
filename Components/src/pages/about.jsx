5b601580160925bfafa156b3fac605c1cda1236e
Janindumaleesha <janindu.maleesha@app360.lk>
2025-07-25 22:56:21 +0530

    lession 10

--- Components/src/pages/about.jsx
+++ Components/src/pages/about.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HeaderContent from '../components/HeaderContent/HeaderContent.jsx'
 
 function about() {
+
+  // let num = 3;
+
+  const [num, setNum] = useState(3);
+
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setNum(4);
+  }
+
+  const increase = () => {
+    setCount(count + 1)
+  }
+
+  const decrease = () => {
+    setCount(count - 1)
+  }
+
+  useEffect(() => {
+    console.log('Hi');
+  },[])
+
+  // let number = 10;
+
+  // if (number == 5)
+  //   alert('Hi there!')
+  // else
+  //   alert('Hi Maleesha!')
+
   return (
     <>
         <HeaderContent />
-        <div>about</div>
+        <div id='wrpper'>
+          <button onClick={handleClick}>Click</button>
+          <p>{num}</p>
+
+          <h3>Counter</h3>
+          <button onClick={increase} style={{width:50,height:20,margin:10}}>+</button>
+          <button onClick={decrease} style={{width:50,height:20}}>-</button>
+          <p>{count}</p>
+        </div>
     </>
   )
 }
